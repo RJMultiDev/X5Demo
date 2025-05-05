79d301b063afa21dde21bc033411db3bd9f44d31
MultiDev <2815968613@qq.com>
2025-05-05 10:49:53 +0800

    更新 DemoApplication.java

--- app/src/main/java/com/tencent/tbs/demo/DemoApplication.java
+++ app/src/main/java/com/tencent/tbs/demo/DemoApplication.java
@@ -65,7 +65,7 @@ public class DemoApplication extends Application {
         File internalStorage = this.getFilesDir();
         String path = internalStorage.getAbsolutePath();
         if (!QbSdk.isTbsCoreInited()) {
-            if (webView.getX5WebViewExtension() != null) {
+            if (QbSdk.getTbsVersion(context) <= 0) {
                 copyAssetsToSDCard(this, "tbs", path + "/tbs");
                 QbSdk.installLocalTbsCore(this, 46904, path + "/tbs" + "/tbs_core_046904_20231225151606_nolog_fs_obfs_armeabi_release.tbs");
             }
