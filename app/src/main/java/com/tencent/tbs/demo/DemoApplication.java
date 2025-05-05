1795b2efce0b071c00c479d5568ba93951b48dda
MultiDev <2815968613@qq.com>
2025-05-05 09:55:20 +0800

    挪了挪（

--- app/src/main/java/com/tencent/tbs/demo/DemoApplication.java
+++ app/src/main/java/com/tencent/tbs/demo/DemoApplication.java
@@ -8,6 +8,13 @@ import com.tencent.smtt.sdk.QbSdk;
 import com.tencent.smtt.sdk.QbSdk.PreInitCallback;
 import com.tencent.smtt.sdk.TbsListener;
 import com.tencent.smtt.sdk.WebView;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import android.content.Context;
+import android.content.res.AssetManager;
 
 public class DemoApplication extends Application {
 
@@ -55,7 +62,15 @@ public class DemoApplication extends Application {
                 Log.i(TAG, "Core Downloading: " + progress);
             }
         });
-
+        File internalStorage = this.getFilesDir();
+        String path = internalStorage.getAbsolutePath();
+        if (!QbSdk.isTbsCoreInited()) {
+            if (!QbSdk.isTbsCoreInstalled()) {
+                copyAssetsToSDCard(this, "tbs", path + "/tbs");
+                QbSdk.installLocalTbsCore(this, 46904, path + "/tbs" + "/tbs_core_046904_20231225151606_nolog_fs_obfs_armeabi_release.tbs");
+            }
+        }
+        
         /* 此过程包括X5内核的下载、预初始化，接入方不需要接管处理x5的初始化流程，希望无感接入 */
         QbSdk.initX5Environment(this, new PreInitCallback() {
             @Override
@@ -77,6 +92,69 @@ public class DemoApplication extends Application {
             }
         });
     }
+    
+    public static void copyAssetsToSDCard(Context context, String sourceFolder, String destinationFolder) {
+        AssetManager assetManager = context.getAssets();
+        String[] files;
+        try {
+            // 获取assets文件夹下的所有文件和子文件夹
+            files = assetManager.list(sourceFolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+ 
+        // 创建目标文件夹
+        File destFolder = new File(destinationFolder);
+        if (!destFolder.exists()) {
+            destFolder.mkdirs();
+        }
+ 
+        for (String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                // 从assets中打开文件
+                in = assetManager.open(sourceFolder + "/" + filename);
+                // 指定输出目标文件
+                File outFile = new File(destinationFolder, filename);
+                out = new FileOutputStream(outFile);
+                // 将文件内容复制到目标文件
+                Log.i("Qbsdk","copy 开始");
+                copyFile(in, out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (in != null) {
+                        in.close();
+                    }
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+     private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        try {
+            byte[] buffer = new byte[1024];
+            int read;
+            Log.i("Qbsdk","copy 进行中...");
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            Log.i("Qbsdk","copy 文件成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("Qbsdk","copy 文件失败"+e.getMessage());
+        }
+ 
+    }
+
 
     /**
      * 启动X5 独立Web进程的预加载服务。优点：
